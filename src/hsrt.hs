2b9c840fea93ba9243ad9d9b1e7cff6f2305c727
Michael Brailsford <brailsmt@yahoo.com>
2013-10-03 19:55:44 -0500

    Ray/Sphere intersection is done.

--- src/hsrt.hs
+++ src/hsrt.hs
@@ -8,26 +8,21 @@ import HSRT.Types
 
 
 -- Subtract one point from another
-diff :: Point -> Point -> Point
-diff p1 p2 = zipWith (-) p2 p1
+diff = zipWith (-) 
 
 -- Euclidian distance between two points in n-space
 distance :: Point -> Point -> Double
-distance p1 p2 = (sqrt . sum . (map (^2))) (diff p1 p2)
+distance p1 p2 = (sqrt . sum . (map (^2))) $ diff p1 p2
 
 -- normalize a ray
 normalize :: Ray -> Ray
-normalize (Ray o d) = Ray [0,0,0] (map (/l) d)
+normalize (Ray o d) = Ray o (map (/l) d)
     where 
         l = (sqrt . sum . (map (^2))) d
 
 -- dot product of two rays
 dot :: Ray -> Ray -> Double
-dot (Ray _ d0) (Ray _ d1) = sum (zipWith (*) d0 d1)
-
-mkray :: Point -> Ray
-mkray p = normalize (Ray [0,0,0] p)
-
+dot (Ray _ d0) (Ray _ d1) = sum $ zipWith (*) d0 d1
 
 -- A Sphere
 data Sphere = Sphere {
@@ -35,13 +30,20 @@ data Sphere = Sphere {
 	radius :: Double
 } deriving (Show, Eq)
 
-intersectSphere :: Ray -> Sphere -> [Double]
-intersectSphere ray sphere = [t1, t2]
-    where
-        v            = mkray ((origin ray) `diff` (center sphere))
-        b            = 2 * (v `dot` v)
-        ac           = 4 * ((v `dot` v) - ((radius sphere)^2))
-        discriminant = sqrt ((b^2) - ac)
-        t1           = (-b - discriminant) * 0.5
-        t2           = (-b + discriminant) * 0.5
+r = normalize $ Ray [1,-2,-1] [1,2,4]
+s = Sphere [3,0,5] 3
 
+_intersection :: Ray -> Double -> Point
+_intersection (Ray o d) t = zipWith (+) o (map (*t) d)
+
+intersection :: Ray -> Sphere -> Point
+intersection ray sphere 
+    | discriminant < 0 = []
+    | t0 >= 0          = _intersection ray t0
+    | otherwise        = _intersection ray t1
+    where 
+        b  = 2 * (sum (zipWith (*) (direction ray) (diff (origin ray) (center sphere))))
+        c  =     (sum $ map (^2) $ diff (origin ray) (center s)) - (radius s)^2
+        discriminant = b^2 - 4*c
+        t0 = (-b - sqrt discriminant)/2
+        t1 = (-b + sqrt discriminant)/2
